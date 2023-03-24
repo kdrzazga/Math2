8a2ac916094d3bde6c0efd118d78df0d3fbd22a8
kdrzazga <krzysztof.drzazga@tutanota.com>
2023-03-24 13:53:07 +0100

    POM fix

---
+++ src/test/java/org/kd/main/App.java
@@ -0,0 +1,10 @@
+package org.kd.main;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class App {
+    public static void main(String[] args) {
+        Logger.getLogger(App.class.getSimpleName()).log(Level.INFO, "It's just a lib, bro");
+    }
+}
